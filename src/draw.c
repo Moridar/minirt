a2ad8256a535544329e951957c570d429beeb1e7
Bobbie Syväsalmi <bsyvasalmi@student.hive.fi>
2024-04-10 16:16:38 +0300

    refactored ray_to_light

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: bsyvasal <bsyvasal@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:49:29 by bsyvasal          #+#    #+#             */
-/*   Updated: 2024/04/10 16:06:28 by bsyvasal         ###   ########.fr       */
+/*   Updated: 2024/04/10 16:16:19 by bsyvasal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ float	ray_to_light(t_vector3 pos, t_vector3 lightpos, t_vector3 normal, t_hitabl
 {
 	t_ray	light;
 	float	dot;
+	float	distance_to_light;
+	t_hitpoint eclipse;
 
 	light.dir = vec3_unit(vec3_sub(lightpos, pos));
 	light.origin = &pos;
+	distance_to_light = vec3_length(vec3_sub(pos, lightpos));
+	eclipse = hit_hitable(list, light);
+	if (eclipse.hit == 1 && eclipse.distance > 0
+		&& eclipse.distance < distance_to_light);
+		return (0);
 	dot = vec3_dot(light.dir, vec3_unit(normal));
-	if (hit_hitable(list, light).hit == 1 && hit_hitable(list, light).distance > 0
-		&& hit_hitable(list, light).distance < vec3_length(vec3_sub(pos, lightpos)))
-	{
-		return (0.3);
-	}
-	if (dot < 0.1)
-		return (0.1);
 	return (dot);
 }
 static int	default_color(t_ray ray)
